39a56c0d3726350cd2949fdcedff8893e478d404
Yash09042004 <yashkiran2004@gmail.com>
2024-07-19 10:38:57 +0530

    amaya

--- CTFd/views.py
+++ CTFd/views.py
@@ -210,7 +210,7 @@ def setup():
         <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="{default_ctf_banner_location}" />
         <h3 class="text-center">
             <p>A cool CTF platform from <a href="https://ctfd.io">ctfd.io</a></p>
-            <p>Follow us on social media:</p>
+            <p>WLUG us on social media:</p>
             <a href="https://twitter.com/ctfdio"><i class="fab fa-twitter fa-2x" aria-hidden="true"></i></a>&nbsp;
             <a href="https://facebook.com/ctfdio"><i class="fab fa-facebook fa-2x" aria-hidden="true"></i></a>&nbsp;
             <a href="https://github.com/ctfd"><i class="fab fa-github fa-2x" aria-hidden="true"></i></a>
